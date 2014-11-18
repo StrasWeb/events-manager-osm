166236fd4518e63340edf2cecd1323e9c2217a75
Pierre Rudloff <contact@rudloff.pro>
2014-11-18 21:09:19 +0100

    Add transport layer

--- events-manager-osm.js
+++ events-manager-osm.js
@@ -1,5 +1,5 @@
 /*global google, jQuery*/
-/*jslint browser: true*/
+/*jslint browser: true, unparam: true*/
 function replaceMap(e, map) {
     'use strict';
     map.mapTypes.set("OSM", new google.maps.ImageMapType({
@@ -10,7 +10,22 @@ function replaceMap(e, map) {
         name: "OpenStreetMap",
         maxZoom: 18
     }));
+    map.mapTypes.set("transport", new google.maps.ImageMapType({
+        getTileUrl: function (coord, zoom) {
+            return "https://a.tile.thunderforest.com/transport/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+        },
+        tileSize: new google.maps.Size(256, 256),
+        name: "OSM Transport",
+        maxZoom: 18
+    }));
     map.mapTypeId = 'OSM';
+    map.setOptions({
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            mapTypeIds: ['OSM', 'transport']
+        }
+
+    });
 }
 
 jQuery(document).bind('em_maps_location_hook', replaceMap);
