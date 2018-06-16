25a4812735bcc266ad4180876822e0c8da9c2014
Pierre Rudloff <contact@rudloff.pro>
2018-06-16 16:48:20 +0200

    refactor: Cleanup main PHP file

--- events-manager-osm.php
+++ events-manager-osm.php
@@ -1,16 +1,4 @@
 <?php
-/**
- * Use OpenStreetMap in WordPress Events Manager.
- *
- * PHP Version 5.4
- *
- * @category Plugin
- *
- * @author   StrasWeb <contact@strasweb.fr>
- * @license  GPL http://www.gnu.org/licenses/gpl.html
- *
- * @link     https://github.com/StrasWeb/events-manager-osm
- */
 /*
 Plugin Name: Events Manager OSM
 Plugin URI: https://github.com/StrasWeb/events-manager-osm
@@ -18,8 +6,12 @@ Description: Use OpenStreetMap in WordPress Events Manager
 Author: StrasWeb
 Version: 0.2.1
 Author URI: https://strasweb.fr/
-*/
+ */
+
+use EventsManagerOsm\ScriptLoader;
 
 require_once __DIR__.'/vendor/autoload.php';
 
-add_action('wp_enqueue_scripts', ['EventsManagerOsm\ScriptLoader', 'init']);
+if (function_exists('add_action')) {
+    add_action('wp_enqueue_scripts', [ScriptLoader::class, 'init']);
+}
